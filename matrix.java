13c54bfcb2f7d4e1b9458ef105c8b680bf1d3c87
nitinkumar-prog <nitinprajapati90846@gmail.com>
2025-08-12 04:27:01 -0700

    one_way_to_repersent_matrix

--- matrix.java
+++ matrix.java
@@ -1,3 +1,20 @@
 public class matrix {
-    
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == 4 || j == 4) {
+                    System.out.print("3 ");
+                }
+                else if(i==1||i==3||j==1||j==3)
+                {
+                    System.out.print("2 ");
+                } else {
+                    System.out.print("1 ");
+                }
+            }
+            System.out.println();
+
+        }
+    }
 }
