aa848e906fa9798b03d180c8fb5ade288080bec1
nitinkumar-prog <nitinprajapati90846@gmail.com>
2025-09-02 15:30:23 +0530

    Reverse_array

---
+++ ReverseArray.java
@@ -0,0 +1,32 @@
+    
+import java.util.Scanner;
+
+public class ReverseArray{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Reverse in place
+        int start = 0, end = n - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+        System.out.print("Reversed array: ");
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+    }
+}
+
