8a5021ca956609728f2c2e43cada35356c4d1063
nitinkumar-prog <nitinprajapati90846@gmail.com>
2025-09-06 22:31:30 +0530

    week1_Q4

---
+++ week1_Q4.c
@@ -0,0 +1,11 @@
+//Write a C program to print the first 10 natural numbers using a for loop. 
+ #include<stdio.h>
+ int main()
+ {
+    printf("first 10 natural numbers are:\n");
+    for(int i=1;i<=10;i++)
+    {
+        printf("%d\t",i);
+    }
+    return 0;
+ }
