a634cd1d8474b9ab400fd9bde468eaf691d4f088
nitinkumar-prog <nitinprajapati90846@gmail.com>
2025-09-07 10:26:55 +0530

    week1_Q14

---
+++ week1_Q13.c
@@ -0,0 +1,21 @@
+//Write a C program to check whether a character is a vowel or consonant using if-else.
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    
+    printf("Enter an alphabet: ");
+    scanf(" %c", &ch);  
+
+    
+
+        // Check for vowels
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'||ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            printf("'%c' is a vowel.\n", ch);
+        } else {
+            printf("'%c' is a consonant.\n", ch);
+        }
+    
+    return 0;
+}
